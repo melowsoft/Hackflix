3a314029be593dd463afdb0001bab6f49e855b56
Anthony Okpako <melowsworld@yahoo.co.uk>
2021-08-25 22:56:39 +0100

    added test

---
+++ test/App.spec.tsx
@@ -0,0 +1,24 @@
+import {shallow} from 'enzyme';
+import React from 'react';
+import  ReactDOM  from 'react-dom';
+import {App} from '../src/App';
+import 'jest-enzyme'
+import { Footer } from '../src/components/Footer';
+import { AppContextProvider } from '../src/context/AppContextProvider';
+import { QueryClient, QueryClientProvider } from 'react-query'
+import {render} from "@testing-library/react"
+
+const queryClient = new QueryClient();
+
+test('App render without crashing', () => {
+   
+    const div = document.createElement('div')
+    ReactDOM.render(
+    <QueryClientProvider client={queryClient}>
+        <AppContextProvider>
+            <App />
+        </AppContextProvider>
+    </QueryClientProvider>, div)
+
+});
+
---
+++ test/MovieList.spec  copy.tsx
@@ -0,0 +1,39 @@
+import {ICategory, IGenre, IMovie} from '../src/types';
+import {shallow} from 'enzyme';
+import React from 'react';
+import {Movie} from '../src/components/Movie';
+import {MovieList} from '../src/components/MovieList';
+import 'jest-enzyme'
+
+
+const harryPotterAndThePhilosophersStoneMovie: IMovie = {
+    id: 1,
+    title: `Harry Potter and the Philosopher's Stone`,
+    genre_ids: [1, 2, 3],
+    backdrop_path: '',
+    poster_path: '/posters/harry-potter-and-the-philosopher-s-stone',
+};
+
+const harryPotterAndTheChamberOfSecretsMovie: IMovie = {
+    id: 2,
+    title: 'Harry Potter and Chamber of Secrets',
+    genre_ids: [1, 2, 3],
+    backdrop_path: '',
+    poster_path: '/posters/harry-potter-and-the-chamber-of-secrets',
+};
+
+
+test('should render multiples movies title and poster', () => {
+    // Given
+    const movies: IMovie[] = [
+        harryPotterAndThePhilosophersStoneMovie,
+        harryPotterAndTheChamberOfSecretsMovie,
+    ]
+    // When
+    const component = shallow(<MovieList movies={movies}/>);
+
+    // Then
+    expect(component).toContainReact(<Movie movie={harryPotterAndThePhilosophersStoneMovie} />)
+    expect(component).toContainReact(<Movie movie={harryPotterAndTheChamberOfSecretsMovie} />)
+});
+
