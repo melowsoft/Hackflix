b0633ebeff3806b542c1fd392ca6e196371afcdd
Davy Engone <davy@hackages.io>
2021-08-26 00:48:41 +0200

    suggested some changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ import React from "react";
 export function Footer() {
   return (
     <footer className="py-6 bg-gray-900">
-    <div className="container mx-auto text-center">
-      <p>Hackflix © 2020 Powered by Hackages</p>
-    </div>
-  </footer>
+      <div className="container mx-auto text-center">
+        <p>Hackflix © 2020 Powered by Hackages</p>
+      </div>
+    </footer>
   );
 }
--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -7,16 +7,15 @@ interface IMovieListProps {
 }
 
 export function MovieList({movies}: IMovieListProps) {
-
-    return (
-        <div className="movie-list py-20">
-        <div className="container mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
-            {movies && movies.map((movie: IMovie, index: number) => (
-              <Movie movie={movie} key={index}/>
-            ))}
-          </div>
+  return (
+    <div className="movie-list py-20">
+      <div className="container mx-auto">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-10">
+          {movies?.map((movie: IMovie, index: number) => (
+            <Movie movie={movie} key={index}/>
+          ))}
         </div>
       </div>
-    );
+    </div>
+  );
 }
